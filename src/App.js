e179b37ef8351ab5e7a1bd2dbaf17484fc5c8c95
Alex <2393097587@qq.com>
2023-11-22 18:08:23 +0800

    refactor: 代码重构

--- src/App.js
+++ src/App.js
@@ -3,98 +3,101 @@ import './index.css'
 
 function App() {
 
-  // 初始化input框输入的值， 并拿到输入的值
-  const [inpValue, setInpValue] = useState('')
+  const [todos, setTodos] = useState(JSON?.parse(localStorage?.getItem("todoList")) || []);
+  const [changeValue, setChangeValue] = useState('');
 
-  // 初始化一个数组, 把增加的todoList添加进来
-  const [todoList, setTodoList] = useState([])
-
-  //增加一个todo
-  const addTodo = () => {
-    const todo = {
-      key: new Date().getTime(),
-      content: inpValue,
-      done: false
-    }
-    setTodoList([...todoList, todo])
-    saveTodoList([...todoList, todo])
-  }
-
-  // 删除一个todo
-  const deleteTodo = (key) => {
-    const todos = [...todoList]
-    const newTodoList = todos.filter(todo => todo.key !== key)
-    setTodoList([...newTodoList])
-    saveTodoList([...newTodoList])
-  }
-
-  // 更新一个todo
-  const updateTodo = (key) => {
-    const todos = [...todoList]
-    todos.forEach(todo => {
-      if (todo.key === key) {
-        if (todo.done === false) {
-          todo.done = true
-        } else {
-          todo.done = false
-        }
-      }
-    })
-    setTodoList([...todos])
-    saveTodoList([...todos])
-  }
-
-  // 实现本地存储, 序列化数组
-  const saveTodoList = (arrTodo) => {
-    localStorage.todoList = JSON.stringify(arrTodo)
-  }
-
-  // 反序列化本地todoList, 解析到页面
-  const loadTodoList = () => {
-    let todos = localStorage.todoList
-    if (!todos) {
-      todos = '[]'
-    }
-    setTodoList(JSON.parse(todos))
-  }
-
+  // 初始化本地数据
   useEffect(() => {
-    loadTodoList()
-    console.log('1111');
+    readTodos();
   }, [])
 
-  const newTime = (time) => {
-    const d = new Date(time)
-    const dataS = d.getDate()
-    const monthS = d.getMonth() + 1
-    const hoursS = d.getHours()
-    const minuteS = d.getMinutes()
-    const secondS = d.getSeconds()
-    const yarn = d.getFullYear();
-    const month = dataS < 10 ? '0' + dataS : dataS;
-    const data = monthS < 10 ? '0' + monthS : monthS;
-    const hours = hoursS < 10 ? '0' + hoursS : hoursS;
-    const minute = minuteS < 10 ? '0' + minuteS : minuteS;
-    const second = secondS < 10 ? '0' + secondS : secondS;
-    return yarn + '-' + month + '-' + data + ' ' + hours + ':' + minute + ':' + second
+  // 监听todos存储到本地
+  useEffect(() => {
+    saveTodos();
+  }, [todos])
+
+  /**
+   * 添加一个todo
+   */
+  const handleAddTodoList = () => {
+    const id = new Date().getTime();
+    // 组装数据
+    const todo = {
+      name: changeValue, //名称
+      id, // 唯一值
+      done: false, // 完成
+    };
+    setTodos([...todos, todo]);
+  }
+
+  /**
+   * 监听输入的值
+   * @param {*} e 
+   */
+  const handleInputValue = (e) => {
+    const { value } = e.target;
+    setChangeValue(value);
+  }
+
+  /**
+   * 删除一个todo
+   * @param {*} id
+   */
+  const handleDeleteTodo = (id) => {
+    const todoFilter = todos.filter(item => item.id !== id);
+    setTodos([...todoFilter]);
+  }
+
+  /**
+   * 完成一个todo
+   * @param {*} id
+   */
+  const handelDoneTodo = (id) => {
+    const todoDone = todos.map(item => {
+      if (item.id === id) {
+        item.done = !item.done;
+      }
+      return item;
+    })
+    setTodos([...todoDone]);
+  }
+
+  /**
+   * 存储本地
+   * @param {*} todo 
+   */
+  function saveTodos() {
+    // 序列化todo  存储本地
+    const todoStr = JSON.stringify(todos);
+    localStorage.setItem("todoList", todoStr);
+  }
+
+  /**
+   * 读取本地数据
+   * @param {*} todo 
+   */
+  function readTodos() {
+    let todoParse = JSON.parse(localStorage.getItem("todoList"));
+    if (!todoParse) {
+      todoParse = [];
+    }
+    setTodos([...todoParse]);
   }
 
   return (
     <div>
-      <input placeholder="请输入内容" onChange={e => setInpValue(e.target.value)} />
-      <button onClick={() => addTodo()}>添加</button>
-      {
-        todoList.map(todo => {
-          return (
-            <div key={todo.key}>
-              <button onClick={() => updateTodo(todo.key)}>完成</button>
-              <button onClick={() => deleteTodo(todo.key)}>删除</button>
-              <span className="todo-time">{newTime(todo.key)}</span>
-              <span className={todo.done ? "todo-done" : "done"}>{todo.content}</span>
-            </div>
-          )
-        })
-      }
+      <input placeholder="请输入" onChange={handleInputValue} />
+      <button onClick={handleAddTodoList}>添加</button>
+      {todos.map((item) => {
+        const { name, id, done } = item;
+        return (
+          <div key={id}>
+            <button style={{ marginRight: '10px' }} onClick={() => handleDeleteTodo(id)}>删除</button>
+            <button style={{ marginRight: '10px' }} onClick={() => handelDoneTodo(id)}>完成</button>
+            <span className={done ? 'item_done' : ''}>{name}</span>
+          </div>
+        )
+      })}
     </div>
   );
 }
