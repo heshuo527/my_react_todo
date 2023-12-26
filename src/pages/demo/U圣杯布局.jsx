a612bf6209c3b79784504655a7d7fed5f073299f
Alex <2393097587@qq.com>
2023-12-26 09:16:36 +0800

    feat: 新增圣杯布局

---
+++ src/pages/demo/U圣杯布局.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import style from './u圣杯布局.css'
+
+const U = () => {
+    return (
+        <div className={style.container}>
+            <div className={style.left}></div>
+            <div className={style.middle}></div>
+            <div className={style.right}></div>
+        </div>
+    );
+}
+
+export default U;
